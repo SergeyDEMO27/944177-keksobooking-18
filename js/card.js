a90bc42a6fdaed04a0b7c983bb0f7d016f701a88
edji777 <49676371+edji777@users.noreply.github.com>
2019-11-11 14:29:24 +0300

    внес исправления

--- js/card.js
+++ js/card.js
@@ -18,12 +18,21 @@
       cardElement.querySelector('.popup__features').appendChild(feature);
     });
     cardElement.querySelector('.popup__description').textContent = card.offer.description;
-    cardElement.querySelector('.popup__photos').querySelector('img').src = card.offer.photos;
+    cardElement.querySelector('.popup__photos').innerHTML = '';
+    card.offer.photos.forEach(function (it) {
+      var photo = document.createElement('img');
+      photo.className = 'popup__photo';
+      photo.setAttribute('width', '45');
+      photo.setAttribute('height', '40');
+      photo.alt = 'Фотография жилья';
+      photo.src = it;
+      cardElement.querySelector('.popup__photos').appendChild(photo);
+    });
     cardElement.querySelector('.popup__avatar').src = card.author.avatar;
     cardElement.querySelector('.popup__close').addEventListener('click', function () {
       cardElement.remove();
     });
-    cardElement.addEventListener('keydown', function (evt) {
+    document.addEventListener('keydown', function (evt) {
       if (evt.keyCode === ESC_KEYCODE) {
         cardElement.remove();
       }
